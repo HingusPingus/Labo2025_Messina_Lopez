f7f4b9bba19155a9bfb05f5233aa9aba3581f194
HingusPingus <167221890+HingusPingus@users.noreply.github.com>
2025-04-20 20:22:35 -0300

    Add files via upload

---
+++ Java/src/unidad1/Customer_Messina.java
@@ -0,0 +1,14 @@
+public class Customer_Messina {
+private String name;
+
+public Customer_Messina(){
+    this.name="pinky";
+}
+public String getName(){
+    return name;
+}
+    public static void main(String args[]){
+        Customer_Messina c1=new Customer_Messina();
+        System.out.println("Nombre: "+c1.getName());
+    }
+}
